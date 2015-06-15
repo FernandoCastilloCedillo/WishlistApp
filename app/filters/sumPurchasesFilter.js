97ac2f5b1aa24f29940b081b90be0af65000917a
Fernando Castillo <superferenabled@gmail.com>
2015-06-15 04:28:40 -0500

    see what’s the total money I’ve spent on my purchased items.

--- app/filters/sumPurchasesFilter.js
+++ app/filters/sumPurchasesFilter.js
@@ -1,9 +1,31 @@
- app.filter('sumPurchasesFilter', function() {
-     return function(groups) {
-         var taxTotals = 0;
-         for (i=0; i<groups.length; i++) {
-             taxTotal = taxTotal + groups[i].taxes;    
-          };
-         return taxTotals;
-     };
- });
+ (function(){
+
+ 	var countPurchasesFilter = function() {
+		return function(groups) {
+			var count = 0;
+			for (i=0; i<groups.length; i++) {
+				count += groups[i].purchased === true ? 1 : 0;    
+			};
+			return count;
+		};
+	}
+
+ 	var sumPurchasesFilter = function() {
+		return function(groups) {
+			var sum = 0;
+			for (i=0; i<groups.length; i++) {
+				sum += groups[i].purchased === true ? groups[i].cost : 0;    
+			};
+			return sum;
+		};
+	}
+
+	var app = angular.module('wishlistApp');
+	app.filter('countPurchasesFilter', countPurchasesFilter);
+	app.filter('sumPurchasesFilter', sumPurchasesFilter);
+
+ }());
+
+
+
+
